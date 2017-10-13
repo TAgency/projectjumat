a4fa07ad199824a3ec172c2d66744f732a408a37
Andrew <32459976+andreewww56@users.noreply.github.com>
2017-10-13 14:37:05 +0700

    add image, configure game

--- Game/game.js
+++ Game/game.js
@@ -1,16 +1,19 @@
 let DARAH = document.getElementById('enemyhealth2').clientWidth //244
+let DARAH2 = document.getElementById('playerhealth2').clientWidth //244
 
-function attack(value) {
+function attack(value,value2) {
     document.getElementById('enemyhealth2').style.width = DARAH - value;
     DARAH -= value;
+    document.getElementById('playerhealth2').style.width = DARAH2 - value;
+    DARAH2 -= value2;
 }
 
 document.getElementById('atk').onclick = function() {
-    attack(40)
+    attack(40,20)
 }
 document.getElementById('atk2').onclick = function() {
-    attack(0)
+    attack(0,40)
 }
 document.getElementById('atk3').onclick = function() {
-    attack(20)
+    attack(20,10)
 }
--- backend/index.js
+++ backend/index.js
@@ -6,25 +6,30 @@ const pokemonCollection = [
     id: 0,
     name: "Bulbasaur",
     type: "Grass Poison",
-    generation: 1
+    generation: 1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/bulbasaur.gif"
+    
   },
   {
     id: 1,
     name: "Charmander",
     type: "Fire",
-    generation:1
+    generation:1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/charmander.gif"
   },
   {
     id: 2,
     name: "Squirtle",
     type: "Water",
-    generation: 1
+    generation: 1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/squirtle.gif"
   },
   {
     id: 3,
     name: "Pikachu",
     type: "Electric",
-    generation: 1
+    generation: 1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/pikachu.gif"
   }
 ]
 // copas untuk eror header
