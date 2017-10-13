f69651420bbaa791a9b45dc8ab01972538c135d1
Andrew <32459976+andreewww56@users.noreply.github.com>
2017-10-13 15:50:11 +0700

    add images index.js backend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const pokemonCollection = [
   {
-    id: 0,
+    id: 1,
     name: "Bulbasaur",
     type: "Grass Poison",
     generation: 1,
@@ -11,21 +11,65 @@ const pokemonCollection = [
     
   },
   {
-    id: 1,
+    id: 2,
+    name: "Ivysaur",
+    type: "Grass Poison",
+    generation: 1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/ivysaur.gif"
+    
+  },
+  {
+    id: 3,
+    name: "Venusaur",
+    type: "Grass Poison",
+    generation: 1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/venusaur.gif"
+    
+  },
+  {
+    id: 4,
     name: "Charmander",
     type: "Fire",
     generation:1,
     image: "https://img.pokemondb.net/sprites/black-white/anim/normal/charmander.gif"
   },
   {
-    id: 2,
+    id: 5,
+    name: "Charmleon",
+    type: "Fire",
+    generation:1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/charmeleon.gif"
+  },
+  {
+    id: 6,
+    name: "Charizard",
+    type: "Fire Flying",
+    generation:1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif"
+  },
+  {
+    id: 7,
     name: "Squirtle",
     type: "Water",
     generation: 1,
     image: "https://img.pokemondb.net/sprites/black-white/anim/normal/squirtle.gif"
   },
   {
-    id: 3,
+    id: 8,
+    name: "Watortle",
+    type: "Water",
+    generation: 1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/wartortle.gif"
+  },
+  {
+    id: 9,
+    name: "Blastoise",
+    type: "Water",
+    generation: 1,
+    image: "https://img.pokemondb.net/sprites/black-white/anim/normal/blastoise.gif"
+  },
+  {
+    id: 10,
     name: "Pikachu",
     type: "Electric",
     generation: 1,
